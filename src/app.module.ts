f409721153eecf0ec341c9284417c6522ecb425a
Fakher Ghouili <fakherghouili@gmail.com>
2024-12-27 19:32:12 +0100

    Add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { ConfigModule } from '@/config/config.module';
+import { DatabaseModule } from '@/database/database.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, DatabaseModule, TerminusModule],
   controllers: [AppController],
   providers: [AppService],
 })
