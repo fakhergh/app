2ee5b08b8a16605812837a33b62e56c705246fd4
Fakher Ghouili <fakherghouili@gmail.com>
2024-12-27 18:45:22 +0100

    Add morgan, graphql upload and config eslint simple sort plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,8 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin', 'simple-import-sort'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   root: true,
   env: {
     node: true,
@@ -17,9 +14,28 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'no-shadow': 'off',
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-var-requires': ['off'],
+    '@typescript-eslint/no-shadow': ['error'],
+    '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        bracketSpacing: true,
+        tabWidth: 2,
+        printWidth: 120,
+        useTabs: false,
+        singleQuote: true,
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { graphqlUploadExpress } from 'graphql-upload';
+import * as morgan from 'morgan';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    morgan('dev', {
+      stream: {
+        write: function (str) {
+          Logger.log(str, 'Request');
+        },
+      },
+    }),
+  );
+  app.enableCors();
+  app.use(
+    graphqlUploadExpress({ maxFileSize: 10_000_000 /*10mb*/ /*maxFiles: 5*/ }),
+  );
+
   await app.listen(9000);
 }
 bootstrap();
